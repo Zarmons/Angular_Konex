54f6a004932fe33d1af86326e7fb53f03e3085db
FRED-SANTIAGO\Fred Santiago Ovallo <fsantiagoovallos@gmail.com>
2023-02-02 00:45:05 -0500

    -ajuste de tabla -creacion del crud -creacion de paginador

--- src/app/feature/dispensary/dispensary/dispensary.component.ts
+++ src/app/feature/dispensary/dispensary/dispensary.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DispensaryService } from 'src/app/feature/dispensary/shared/services/dispensary.service';
+import { Dispensary } from '../shared/models/dispensary';
+
 @Component({
   selector: 'app-dispensary',
   templateUrl: './dispensary.component.html',
@@ -7,38 +9,145 @@ import { DispensaryService } from 'src/app/feature/dispensary/shared/services/di
 })
 export class DispensaryComponent {
   display: boolean = false;
-
-  data = {
-    name: '',
-    laboratory: '',
-    manufacture: '',
-    expiration: '',
-    quantity: '',
-    value: '',
-  };
+  columns: any[] = [];
+  loading: boolean = true;
+  @ViewChild('dt', { static: false }) dt: any;
 
   constructor(private dispensaryService: DispensaryService) {}
+
+  ngOnInit() {
+    this.columns = [
+      { field: 'name', header: 'Nombre' },
+      { field: 'laboratory', header: 'Laboratorio' },
+      { field: 'expiration', header: 'Fecha de fabricación' },
+      { field: 'manufacture', header: 'Fecha de vencimiento' },
+      { field: 'stock', header: 'Cantidad en stock' },
+      { field: 'value', header: 'Valor unitario' },
+    ];
+    this.loading = false;
+    console.log(this.dispensarys.values)
+    // this.getMedicamentos();
+  }
+
+  dispensarys: Dispensary[] = [
+    {
+      name: 'medicamento 1',
+      laboratory: 'laboratorio 1',
+      expiration: '01/20/23',
+      manufacture: '01/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 2',
+      laboratory: 'laboratorio 2',
+      expiration: '02/20/23',
+      manufacture: '02/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 3',
+      laboratory: 'laboratorio 3',
+      expiration: '03/20/23',
+      manufacture: '03/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 1',
+      laboratory: 'laboratorio 1',
+      expiration: '01/20/23',
+      manufacture: '01/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 2',
+      laboratory: 'laboratorio 2',
+      expiration: '02/20/23',
+      manufacture: '02/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 3',
+      laboratory: 'laboratorio 3',
+      expiration: '03/20/23',
+      manufacture: '03/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 1',
+      laboratory: 'laboratorio 1',
+      expiration: '01/20/23',
+      manufacture: '01/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 2',
+      laboratory: 'laboratorio 2',
+      expiration: '02/20/23',
+      manufacture: '02/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 3',
+      laboratory: 'laboratorio 3',
+      expiration: '03/20/23',
+      manufacture: '03/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 1',
+      laboratory: 'laboratorio 1',
+      expiration: '01/20/23',
+      manufacture: '01/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 2',
+      laboratory: 'laboratorio 2',
+      expiration: '02/20/23',
+      manufacture: '02/25/26',
+      stock: 50,
+      value: 1000,
+    },
+    {
+      name: 'medicamento 3',
+      laboratory: 'laboratorio 3',
+      expiration: '03/20/23',
+      manufacture: '03/25/26',
+      stock: 50,
+      value: 1000,
+    },
+  ];
+
+  // getMedicamentos() {
+  //   this.dispensaryService.get().subscribe(
+  //     (res) => {
+  //       console.log(res);
+  //     },
+  //     (err) => {
+  //       console.log(err);
+  //     }
+  //   );
+  // }
 
   showDialog() {
     this.display = true;
   }
 
-  ngOnInit(): void {
-    this.getMedicamentos();
-  }
-
-  getMedicamentos() {
-    this.dispensaryService.get().subscribe(
-      (res) => {
-        console.log(res);
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
-  }
-
   onData() {
-    console.log(this.data)
+    // console.log(this.data)
+  }
+
+  deleteDrug(index: number) {
+    this.dispensarys.splice(index, 1)
   }
 }
--- src/app/feature/dispensary/shared/models/dispensary.ts
+++ src/app/feature/dispensary/shared/models/dispensary.ts
@@ -1,2 +1,8 @@
 export interface Dispensary {
+  name: string,
+  laboratory: string,
+  expiration: string,
+  manufacture: string,
+  stock: number,
+  value: number,
 }
