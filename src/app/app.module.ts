2c81982def11de4a1859e99dd09bfc26c7a78d8f
Zarmons <fsantiagoovallos@gmail.com>
2023-03-31 11:26:26 -0500

    ajuste para formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
 
@@ -9,8 +10,7 @@ import { NavBarComponent } from './shared/layout/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [AppComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, FormsModule ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
